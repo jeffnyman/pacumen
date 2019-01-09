f1602e3e9175371f4f07e4085c53a690ef8dbe8f
Jeff Nyman <jeffnyman@gmail.com>
2019-01-08 19:00:25 -0600

    Walls and dots are recognized in layout.

---
+++ pacumen/mechanics/grid.py
@@ -0,0 +1,62 @@
+class Grid:
+    def as_list(self, key=True):
+        """
+        Returns a list of grid positions from the current grid, based on
+        the key value.
+        """
+        grid_list = []
+
+        for x in range(self.width):
+            for y in range(self.height):
+                if self[x][y] == key:
+                    grid_list.append((x, y))
+
+        return grid_list
+
+    def __init__(self, width, height, initial_value=False):
+        if initial_value not in [False, True]:
+            raise Exception("Grids can only contain boolean values.")
+
+        self.width = width
+        self.height = height
+
+        self.data = [[initial_value for _ in range(height)] for _ in range(width)]
+
+    def __str__(self):
+        out = [[str(self.data[x][y])[0] for x in range(self.width)] for y in range(self.height)]
+        out.reverse()
+        return '\n'.join([''.join(x) for x in out])
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def __hash__(self):
+        """
+        The hash for a grid will be given in powers of 2. That value will
+        then be added to a hash_value being stored for each element of
+        the grid that is marked True. For example, consider the following
+        grid of dots
+
+        FFFFFFFFFF
+        FTTFFFFFFF
+        FFFFFFFFFF
+
+        Here there are two True elements, so those would be given hash_value
+        and base values accordingly:
+
+        True 0 16
+        True 16 128
+
+        The hash for that grid would then be the addition of both: 144.
+        """
+        base = 1
+        hash_value = 0
+
+        for row in self.data:
+            for element in row:
+                if element:
+                    hash_value += base
+
+                base *= 2
+
+        return hash(hash_value)
--- pacumen/mechanics/layout.py
+++ pacumen/mechanics/layout.py
@@ -1,9 +1,44 @@
 import os
 
+from pacumen.mechanics.grid import Grid
+
 
 class Layout:
+    def process_layout_text(self, layout_text):
+        """
+        The width (x) is how many elements are in a single cell. The
+        height (y) is how many cells there are. So this bit of logic
+        counts down the rows and, for each row, gets each element of
+        that row.
+        """
+        maximum_y = self.height - 1
+
+        for y in range(self.height):
+            for x in range(self.width):
+                layout_char = layout_text[maximum_y - y][x]
+                self.process_layout_characters(x, y, layout_char)
+
+    def process_layout_characters(self, x, y, character):
+        if character == "%":
+            self.walls[x][y] = True
+        elif character == ".":
+            self.dots[x][y] = True
+
+    def is_wall(self, position):
+        row, column = position
+        return self.walls[row][column]
+
     def __init__(self, layout_text):
         self.layout_text = layout_text
+        self.width = len(layout_text[0])
+        self.height = len(layout_text)
+        self.walls = Grid(self.width, self.height, False)
+        self.dots = Grid(self.width, self.height, False)
+        self.process_layout_text(layout_text)
+        self.total_dots = len(self.dots.as_list())
+
+    def __str__(self):
+        return "\n".join(self.layout_text)
 
 
 def load_layout(fullname):
@@ -50,3 +85,19 @@ def test():
     game_layout = get_layout("testing")
     print("Layout as read from file:")
     print(game_layout)
+    print("----------------")
+    print("Width:", game_layout.width)
+    print("Height:", game_layout.height)
+    print("----------------")
+    print("Walls and Dots are Grids")
+    print("----------------")
+    print("Walls:\n{}".format(game_layout.walls))
+    print("Is 0,0 a wall? (True) -", game_layout.is_wall((0, 0)))
+    print("Is 1,1 a wall? (False) -", game_layout.is_wall((1, 1)))
+    print("Hash of walls:", hash(game_layout.walls))
+    print("----------------")
+    print("Dots:\n{}".format(game_layout.dots))
+    print("Hash of dots:", hash(game_layout.dots))
+    print("Total Dots:", game_layout.total_dots)
+    print("Dot Locations:", game_layout.dots.as_list())
+    print("----------------")
